d977d68ffe68c6923823b4cb93b06b11d1032b34
Sanu <sanu.bhumca2014@gmail.com>
2024-03-21 22:00:27 +0530

    Inheritance examples

---
+++ inheritance.js
@@ -0,0 +1,20 @@
+//With the help of prototype we can acheive inheritace in javascript
+
+const object = {
+    name: "Sanu",
+    city: "Bangalore",
+    phone: "54556454",
+    getInfo: function () {
+        console.log(this.name)
+        console.log(`${this.name}  from  ${this.city}`)
+    }
+}
+
+const object2 = {
+    name: "Kumar",
+    city: "Patna"
+}
+
+object2.__proto__ = object
+
+console.log(object2.getInfo())
